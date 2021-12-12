57c5a45eb2fc2c277c794c1fd44fea98813c9cf8
t11s <greenfeatherhelp@gmail.com>
2021-12-12 12:10:48 -0800

    feat: framework

--- src/SampleContract.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.10;
-
-contract SampleContract {
-    function func1(uint256 num) external {}
-
-    function func2(uint256 num) external {}
-}
