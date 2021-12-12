7f70d52b48be83d31147600e5612f860fb231ade
t11s <greenfeatherhelp@gmail.com>
2021-12-12 12:13:15 -0800

    feat: master draft

--- src/BoostMaster.sol
+++ src/BoostMaster.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.10;
 
-contract BoostSafe {}
+import {BoostSafe} from "./BoostSafe.sol";
+
+contract BoostMaster {
+    event BoostSafeCreated(address indexed creator, BoostSafe safe);
+
+    function createSafe() external returns (BoostSafe safe) {
+        safe = new BoostSafe();
+
+        emit BoostSafeCreated(msg.sender, safe);
+    }
+}
