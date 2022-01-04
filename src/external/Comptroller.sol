2695caebb06772bc52a1d79a18e23216cc13df89
t11s <greenfeatherhelp@gmail.com>
2022-01-03 17:39:52 -0800

    📝 More documentation

--- src/external/Comptroller.sol
+++ src/external/Comptroller.sol
@@ -5,6 +5,12 @@ import {ERC20} from "solmate/tokens/ERC20.sol";
 
 import {CERC20} from "./CERC20.sol";
 
+/// @title Comptroller
+/// @author Compound Labs and Rari Capital
+/// @notice Minimal Compound/Fuse Comptroller interface.
 interface Comptroller {
-    function cTokensByUnderlying(ERC20) external view returns (CERC20);
+    /// @notice Maps underlying tokens to their equivalent cTokens in a pool.
+    /// @param token The underlying token to find the equivalent cToken for.
+    /// @return The equivalent cToken for the given underlying token.
+    function cTokensByUnderlying(ERC20 token) external view returns (CERC20);
 }
